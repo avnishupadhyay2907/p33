b26c8f31e05f83ec91c76f2f098a348938987c71
avnishupadhyay2907 <uavnish2907@gmail.com>
2025-05-12 19:25:14 +0530

    done

--- project_3/src/main/java/in/co/rays/project_3/controller/JasperCtl.java
+++ project_3/src/main/java/in/co/rays/project_3/controller/JasperCtl.java
@@ -38,42 +38,48 @@ public class JasperCtl extends BaseCtl {
 	*/
 	private static final long serialVersionUID = 1L;
 
-	@Override protected void doGet(HttpServletRequest request,
-		  HttpServletResponse response) throws ServletException, IOException { try {
-		  
-		 /* / Compilation of jrxml file / */JasperReport jasperReport =
-		  JasperCompileManager
-		  .compileReport("C:\\Users\\uavni\\JaspersoftWorkspace\\MyReports\\pro.jrxml")
-		  ;
-		  
-		  HttpSession session = request.getSession(true); UserDTO dto = (UserDTO)
-		  session.getAttribute("user"); dto.getFirstName(); dto.getLastName();
-		  
-		  Map<String, Object> map = new HashMap<String, Object>(); map.put("ID", 1l);
-		  java.sql.Connection conn = null;
-		  
-		  ResourceBundle rb =
-		  ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
-		  
-		  String Database = rb.getString("DATABASE");
-		  
-		  if ("Hibernate".equalsIgnoreCase(Database)) { conn = ((SessionImpl)
-		  HibDataSource.getSession()).connection(); }
-		  
-		  if ("JDBC".equalsIgnoreCase(Database)) { conn =
-		  JDBCDataSource.getConnection(); }
-		  
-		 /* / Filling data into the report /*/ JasperPrint jasperPrint =
-		  JasperFillManager.fillReport(jasperReport, map, conn);
-		  
-			/* / Export Jasper report / */ byte[] pdf =
-		  JasperExportManager.exportReportToPdf(jasperPrint);
-		  
-		  response.setContentType("application/pdf");
-		  response.getOutputStream().write(pdf); response.getOutputStream().flush(); }
-		  catch (Exception e) {
-		  
-		  } }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			/* / Compilation of jrxml file / */
+			JasperReport jasperReport = JasperCompileManager
+					.compileReport("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\p33@tmp\\pro.jrxml");
+
+			HttpSession session = request.getSession(true);
+			UserDTO dto = (UserDTO) session.getAttribute("user");
+			dto.getFirstName();
+			dto.getLastName();
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("ID", 1l);
+			java.sql.Connection conn = null;
+
+			ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
+
+			String Database = rb.getString("DATABASE");
+
+			if ("Hibernate".equalsIgnoreCase(Database)) {
+				conn = ((SessionImpl) HibDataSource.getSession()).connection();
+			}
+
+			if ("JDBC".equalsIgnoreCase(Database)) {
+				conn = JDBCDataSource.getConnection();
+			}
+
+			/* / Filling data into the report / */ JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,
+					map, conn);
+
+			/* / Export Jasper report / */ byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+
+			response.setContentType("application/pdf");
+			response.getOutputStream().write(pdf);
+			response.getOutputStream().flush();
+		} catch (Exception e) {
+
+		}
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
